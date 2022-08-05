0f75255ec9835230235ac419c142a35c76b80e68
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-05 15:04:42 +0000

    [Add]3-1投稿フォームを作成する まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,7 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する
+    @list=List.new
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  post'lists'=>'lists#create'  #データを追加するためのルーティングを追記した
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
