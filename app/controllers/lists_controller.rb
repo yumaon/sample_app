215a975bbe43b55fcb4cc8917e630a8aa8dec456
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-06 09:11:24 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,22 @@ class ListsController < ApplicationController
     @list=List.new
   end
 
+  # createアクション内の処理の流れは以下↓
+    # 1.フォームに記述した文字やデータを受け取る
+    # 2.受け取ったデータを元にインスタンスを作成する
+    # 3.データをデータベースに保存するためのsaveメソッド実行
+    # 4.トップ画面へ、リダイレクト
+
+  def create
+    # 1&2 データを受け取り新規登録するためのインスタンス作成
+    list=List.new(list_params)
+    # 3 データをデータベースに保存するためのsaveメソッドを実行
+    list.save
+    # 4 トップ画面へリダイレクト
+    redirect_to'/top'
+  end
+
+
   def index
   end
 
@@ -12,4 +28,15 @@ class ListsController < ApplicationController
 
   def edit
   end
+  # ↓privateは”ここから下は一このcontrollerの中でしか呼び出せません”と言う意味がある。
+  # 他のアクションを巻き込まないように下に記述する。
+  private
+  #ストロングパラメータ
+  #マスアサインメント脆弱性を防ぐ仕組み
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+  # params...formから送られてくるデータはparamsの中に入っています。
+  # require...送られてきたデータの中からモデル名（ここではlist)を指定し、データを絞り込む。
+  # permit...requireで絞り込んだデータの中から、保存を許可するカラムを指定する。
 end
